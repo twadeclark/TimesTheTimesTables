a2e7cbe2223aa9b01782b8f31d81e7f2cdec4980
twadeclark <115502590+twadeclark@users.noreply.github.com>
2023-03-15 10:06:53 -0700

    UI cleanup.

--- app/src/main/java/com/example/timesthetimestables/LeaderboardActivity.java
+++ app/src/main/java/com/example/timesthetimestables/LeaderboardActivity.java
@@ -5,14 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LeaderboardActivity extends AppCompatActivity {
+
+    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
+
+        backButton = (Button) findViewById(R.id.backLeaderButton);
+        backButton.setOnClickListener(view -> finish());
 
         getSupportActionBar().setTitle("Leaderboard");
 
